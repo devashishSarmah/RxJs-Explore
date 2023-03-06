25ab59f5c4c795f58ecb666ef44c4e890da3424b
devashishSarmah <88076633+devashishSarmah@users.noreply.github.com>
2023-03-06 19:16:24 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,59 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { from, map, Observable, switchMap, tap } from 'rxjs';
+
+type User = {
+  id: number;
+  name: string;
+};
+
+type UserDetail = {
+  id: User['id'];
+  age: number;
+};
+
+const Users: User[] = [];
+const UserDetails: UserDetail[] = [];
 
 @Component({
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+    <ul>
+    <li *ngFor="let user of users$ | async">{{ user.name }}</li>
+    </ul>
   `,
 })
-export class App {
-  name = 'Angular';
+export class App implements OnInit {
+  users$: Observable<(User & UserDetail)[]>;
+  ngOnInit(): void {
+    Array(10)
+      .fill(1)
+      .forEach((val, index) => {
+        Users.push({ id: index + 1, name: 'User ' + (index + 1) });
+        UserDetails.push({ id: index + 1, age: (Math.random() * 70) | 0 });
+      });
+
+    this.users$ = this.getUsers().pipe(
+      switchMap((user: User) =>
+        this.getUserDetailsById(user.id).pipe(
+          map((userDetail: UserDetail) => Object.assign(userDetail, user))
+        )
+      ),
+      tap(console.log)
+    );
+  }
+
+  getUsers(): Observable<User> {
+    return from(Users);
+  }
+
+  getUserDetailsById(id: User['id']): Observable<UserDetail> {
+    return from(UserDetails.filter((userDetail) => userDetail.id === id));
+  }
 }
 
 bootstrapApplication(App);
